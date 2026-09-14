1b8515bea685bf30d6f71f57f0343d1e00d1cdb1
sirlma111216-boop <sirlma111216@gmail.com>
2026-09-15 01:11:02 +0900

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles.css';
+import './clay.css';
+import './cloud.css';
+createRoot(document.getElementById('root')!).render(<App />);
